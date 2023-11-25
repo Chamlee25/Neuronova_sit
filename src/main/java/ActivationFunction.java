75684db92a4b549488aa3fbd7451478a080f6104
Chamlee25 <fandicek7@gmail.com>
2023-11-25 22:57:40 +0100

    Enhanced switch replacement

--- src/main/java/ActivationFunction.java
+++ src/main/java/ActivationFunction.java
@@ -2,31 +2,19 @@ public class ActivationFunction {
     //ID of activation function
     int AF_ID = 0;
     public ActivationFunction(String name){
-        switch (name.toLowerCase()){
-            case "sigmoid":
-                AF_ID = 1;
-                break;
-            case "tanh":
-                AF_ID = 2;
-                break;
-            case "relu":
-                AF_ID = 3;
-                break;
-            case "softmax":
-                AF_ID = 4;
-                break;
+        switch (name.toLowerCase()) {
+            case "sigmoid" -> AF_ID = 1;
+            case "tanh" -> AF_ID = 2;
+            case "relu" -> AF_ID = 3;
+            case "softmax" -> AF_ID = 4;
         }
     }
     public double activate(double neuronOutput){
-        switch (AF_ID){
-            case 1:
-                return (1 / ( 1 + Math.exp(-neuronOutput)));
-            case 2:
-                return  Math.tanh(-neuronOutput);
-            case 3:
-                return Math.max(0,neuronOutput);
-
-        }
-        return 0;
+        return switch (AF_ID) {
+            case 1 -> (1 / (1 + Math.exp(-neuronOutput)));
+            case 2 -> Math.tanh(-neuronOutput);
+            case 3 -> Math.max(0, neuronOutput);
+            default -> 0;
+        };
     }
 }
